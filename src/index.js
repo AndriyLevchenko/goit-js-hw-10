095eeafed3bdb2859cd5c13a130a58a8e54dc21e
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-09-06 01:13:39 +0300

    goit-js-hw-10

--- src/index.js
+++ src/index.js
@@ -2,43 +2,61 @@ import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+var debounce = require('lodash.debounce');
+
+const DEBOUNCE_DELAY = 300;
+
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const inputNameCountry = document.querySelector('#search-box');
-console.log(inputNameCountry);
 
-inputNameCountry.addEventListener('input', onSearch);
+inputNameCountry.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+// Пошуковий запит
 
 function onSearch (e) {
-    e.preventDefault();
-    const entry = inputNameCountry.value;
-    if(entry.length === 1) {
-        Notify.info(
-            `Too many matches found. Please enter a more specific name.`
-        );
+    const entry = inputNameCountry.value.trim();
+    cleanMarkup();   
+    if (entry !== '') {
+        fetchCountries(entry)
+        .then(country => {      
+            if (country.length > 10) {
+                Notify.info('Too many matches found. Please enter a more specific name.');
+            } else if (country.length === 0) {
+                Notify.failure('Oops, there is no country with that name');
+            } else if (country.length >= 2 && country.length <= 10) {
+                renderCountryList(country);
+                Notify.warning('Select a country to learn more')
+            } else if (country.length === 1) {
+                renderCountryCard(country);
+                Notify.success('Detailed information is displayed')
+            }
+        })
+        .catch(onFetchError)
+        .finally(() => console.log('Detailed information is displayed'));
     }
-    console.log(entry);
-    
-    fetchCountries(entry)
-    .then(renderCountryCard)
-    .catch(onFetchError)
-    // .finally(() => url.reset());
+}
+
+// Створення та рендер розмітки
+
+function renderCountryList(country) {
+    const markup = country.map(({flags, altSpellings}) => {
+        return `
+        <li class="card-svg" style = "display: flex">
+            <img src="${flags.svg}" alt="${altSpellings[1]}" style = "margin: 1px 0px 1px 15px; width: 3%;">
+            <p class="card-title" style = "margin: 0px 0px 0px 10px">${altSpellings[1]}</p>
+        </li>`
+    }).join('');
+    console.log(country);
+    countryList.innerHTML = markup;
 }
 
 function renderCountryCard(country) {
-    const marcup = country.map(({flags, altSpellings, capital, population, languages}) => {
-    if(country.length > 1) {
-        return `
-        <div class="card-svg" style = "display: flex">
-            <img src="${flags.svg}" alt="${altSpellings[1]}" style = "margin: 1px 0px 1px 15px; width: 3%;">
-            <p class="card-title" style = "margin: 0px 0px 0px 10px">${altSpellings[1]}</p>
-        </div>
-        `
-    } else {
+    const markup = country.map(({flags, altSpellings, capital, population, languages}) => {
         return `
         <div class="card" style = "border: solid; width: 300px">
             <div class="card-svg" style = "display: flex">
-                <img src="${flags.svg}" alt="${altSpellings[1]}" style = "margin-left: 15px; width: 30px">
+                <img src="${flags.svg}" alt="${altSpellings[1]}" style = "margin-left: 15px; width: 10%">
                 <h2 class="card-title" style = "margin-left: 10px">${altSpellings[1]}</h2>
             </div>
             <div class="card-description" style = "margin-left: 15px">
@@ -46,25 +64,28 @@ function renderCountryCard(country) {
                 <p class="card-text">Population: ${population}</p>
                 <p class="card-text">Languages: ${Object.values(languages)}</p>
             </div>
-        </div>
-        `
-    }
-    
+        </div>`
     }).join('');
-    countryInfo.innerHTML = marcup;
     console.log(country);
-    // console.log(marcup);
+    countryInfo.innerHTML = markup;
+}
+
+// Функція очистки розмітки
+
+function cleanMarkup() {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
 }
 
 function onFetchError (error) {
     Notify.failure(
         `Oops, there is no country with that name`
-      );
+    );
 }
 
-const DEBOUNCE_DELAY = 300;
 
 
 
-// Створення та рендер розмітки
+
+
 
